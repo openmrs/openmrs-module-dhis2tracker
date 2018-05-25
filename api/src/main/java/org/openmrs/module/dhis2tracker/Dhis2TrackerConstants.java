d88cdb0191479aaf716651a1600566a2db49ef0a
wyclif <wyclif@openmrs.org>
2018-05-25 14:57:07 -0400

    Renamed the global property names for the male and female option in dhis2

--- api/src/main/java/org/openmrs/module/dhis2tracker/Dhis2TrackerConstants.java
+++ api/src/main/java/org/openmrs/module/dhis2tracker/Dhis2TrackerConstants.java
@@ -69,8 +69,8 @@ public class Dhis2TrackerConstants {
 	
 	public static final String GP_ATTRIB_DATE_OF_HIV_DIAGNOSIS_UID = MODULE_ID + ".dateOfHivDiagnosisUID";
 	
-	public static final String GP_OPTION_FEMALE_UID = MODULE_ID + ".maleOptionUID";
+	public static final String GP_OPTION_FEMALE_UID = MODULE_ID + ".maleOptionCode";
 	
-	public static final String GP_OPTION_MALE_UID = MODULE_ID + ".femaleOptionUID";
+	public static final String GP_OPTION_MALE_UID = MODULE_ID + ".femaleOptionCode";
 	
 }
